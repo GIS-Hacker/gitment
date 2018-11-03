f540618bbfa82ae8e1cb110f0a53a887324a20c3
GIS-Hacker <31750841+GIS-Hacker@users.noreply.github.com>
2018-11-03 11:09:21 +0800

    Update gitment.js

--- src/gitment.js
+++ src/gitment.js
@@ -99,7 +99,7 @@ class Gitment {
       }, options)
 
       this.state.user.isLoggingIn = true
-      http.post('https://gh-oauth.imsun.net', {
+      http.post('https://gitment.cs-tao.cc', {
           code,
           client_id,
           client_secret,
